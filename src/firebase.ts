7a60f066a360462551b0bb82b8e2a59c8eb2da57
Your Name <your.email@example.com>
2024-07-23 04:10:30 +0530

    Updated Firebase config to use environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1IfGqbxBxYy24vMJ13xW4aeex9m82_is",
-  authDomain: "no-7-data.firebaseapp.com",
-  databaseURL: "https://no-7-data-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "no-7-data",
-  storageBucket: "no-7-data.appspot.com",
-  messagingSenderId: "260141707398",
-  appId: "1:260141707398:web:0fb24f4be551e0d85ba107"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
