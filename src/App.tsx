d239ca2a64fb4b0d68951f8a79591c559ad9bc21
khuman_ama <151316592+SittingMax@users.noreply.github.com>
2024-07-23 22:22:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ function App() {
           <VStack spacing={4}>
             <Image src="/logo.png" alt="NO7 Logo" boxSize="60px" />
             <Heading as="h1" size="lg" color="white" textAlign="center">
-              Ereicha Singgi Camapign
+              EREICHA SINGGI CAMPAIGN
             </Heading>
             <Text fontSize="sm" color="gray.400" textAlign="center">
               Crafted by iManipur Team
             </Text>
-            <Text fontSize="md" fontWeight="bold" color="white" textAlign="center">
+            <Text fontSize="md" fontWeight="normal" color="white" textAlign="center">
               Achumba Information Mensinbi O
             </Text>
             <RegistrationForm />
